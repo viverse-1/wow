b68ebbda6dd7368eb01aa318664ad8b09fc89338
Vivek Aaryan <vivekk88042@gmail.com>
2022-10-13 14:55:41 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log('Its working')
+
+function myFunction() {
+	alert("The Project is yet to be published.");
+  }
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+	setTheme('light')
+}else{
+	setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+
+for (var i=0; themeDots.length > i; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode
+		console.log('Option clicked:', mode)
+		setTheme(mode)
+	})
+}
+
+function setTheme(mode){
+	if(mode == 'green'){
+		document.getElementById('theme-style').href = 'default.css'
+	}
+
+	if(mode == 'blue'){
+		document.getElementById('theme-style').href = 'blue.css'
+	}
+
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = 'light.css'
+	}
+
+	if(mode == 'purple'){
+		document.getElementById('theme-style').href = 'purple.css'
+	}
+
+	localStorage.setItem('theme', mode)
+}
